b3fc130207e21ba21f08568f3bfbda6c266a48b4
ex-zhousiting <ex-zhousiting@DS-TS-0246.dashuf.local>
2019-08-21 18:24:10 +0800

    push

--- src/main/java/com/laizhong/hotel/mapper/AgainCheckinInfoMapper.java
+++ src/main/java/com/laizhong/hotel/mapper/AgainCheckinInfoMapper.java
@@ -1,9 +1,10 @@
 package com.laizhong.hotel.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.laizhong.hotel.model.AgainCheckinInfo;
-
+@Mapper
 public interface AgainCheckinInfoMapper {
     int insert(AgainCheckinInfo record);
 
