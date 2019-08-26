9b533ea16e2a9063d57923efffc96444ed1fba44
ex-zhousiting <ex-zhousiting@DS-TS-0246.dashuf.local>
2019-08-26 18:10:40 +0800

    push

--- src/main/java/com/laizhong/hotel/config/MyWebMvcConfigurer.java
+++ src/main/java/com/laizhong/hotel/config/MyWebMvcConfigurer.java
@@ -8,10 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyWebMvcConfigurer implements WebMvcConfigurer   {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        //和页面有关的静态目录都放在项目的static目录下
-    	registry.addResourceHandler("/static/**").addResourceLocations("file:/static/");   
-        //registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");     	 
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {        
+    	String os = System.getProperty("os.name");
+    	if (os.toLowerCase().startsWith("win")) {  //如果是Windows系统
+    		   registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    	} else {  //linux 和mac
+	    	registry.addResourceHandler("/static/**").addResourceLocations("file:/root/apps/hotel/static/");
+	    	 
+    	}
     }
  
 }
